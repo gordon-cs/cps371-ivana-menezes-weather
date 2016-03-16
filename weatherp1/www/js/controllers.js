69a057eec4145466421d5850c18be2a7997dc3ea
ivanamenezes <ivana.menezes@gordon.edu>
2016-03-15 21:57:34 -0500

    Added files via upload

--- weatherp1/www/js/controllers.js
+++ weatherp1/www/js/controllers.js
@@ -2,14 +2,21 @@ weatherApp.controller('MainCtrl', function ($scope, $state, WeatherData) {
 	var latitude  = 42.589611;
 	var longitude = -70.819806;
 
+	var weather = this;
+
 	//call getCurrentWeather method in factory
 	WeatherData.getCurrentWeather(latitude,longitude).then(function (resp) {
 	    $scope.weather = resp.data;
+	    //$scope.todayName = getTodayName();
 	    console.log('GOT CURRENT', $scope.weather);
 	}, function(error) {
 	    alert('Unable to get current conditions');
 	    console.error(error);
 	});
+
+	 //weather.forecast = WeatherData;
+
+
     }
 );
 
